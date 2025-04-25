85e86fd81426fbca7dce209208a4e9cb28340e40
mayurrajput8486 <mayurrajput8486@gmail.com>
2025-04-25 08:18:58 +0530

    validation

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -33,6 +33,7 @@ const Register = () => {
     setCourse(selectCourse)
     const selectFees = courseFees[selectCourse]
     setValue("fees",selectFees)
+    
 
   }
 
@@ -57,23 +58,50 @@ const Register = () => {
                         <div className="col-md-6 mb-4">
                           <div data-mdb-input-init className="form-outline">
                             <input type="text" id="form3Example1m" className="form-control form-control-lg" 
-                            {...register('first_name')}/>
+                            {...register('first_name',{
+                              required : "First Name is Required",
+                              pattern : {
+                                value : /^[A-Za-z]+$/,
+                                message : "Only Alphbates are allowed"
+                              }
+                            })}/>
                             <label className="form-label" htmlFor="form3Example1m">First name</label>
+                            {
+                              errors.first_name && <div className='text-danger'>{errors.first_name.message}</div>
+                            }
                           </div>
                         </div>
                         <div className="col-md-6 mb-4">
                           <div data-mdb-input-init className="form-outline">
                             <input type="text" id="form3Example1n" className="form-control form-control-lg" 
-                            {...register('last_name')}/>
+                            {...register('last_name',{
+                              required : "Last Name is required",
+                              pattern : {
+                                value : /^[A-Za-z]+$/,
+                                message : "Only Alphabates are allowed"
+                              }
+                            })}/>
                             <label className="form-label" htmlFor="form3Example1n">Last name</label>
+                            {
+                              errors.last_name && <div className='text-danger'>{errors.last_name.message}</div>
+                            }
                           </div>
                         </div>
                       </div>
 
                       <div data-mdb-input-init className="form-outline mb-4">
                         <input type="text" id="form3Example8" className="form-control form-control-lg" 
-                        {...register('address')}/>
+                        {...register('address',{
+                          required : "Address is required",
+                          pattern : {
+                            value : /^[A-Za-z\d\s,.-]+$/,
+                            message : "Address is Invalid"
+                          }
+                        })}/>
                         <label className="form-label" htmlFor="form3Example8">Address</label>
+                        {
+                          errors.address && <div className='text-danger'>{errors.address.message}</div>
+                        }
                       </div>
 
                       <div className="d-md-flex justify-content-start align-items-center mb-4 py-2">
@@ -81,17 +109,26 @@ const Register = () => {
                         <h6 className="mb-0 me-4">Gender: </h6>
 
                         <div className="form-check form-check-inline mb-0 me-4">
-                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender" value="female" {...register('gender')}/>
+                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender" value="female" 
+                          {...register('gender',{
+                            required : "Please Select ine Option"
+                          })}/>
                           <label className="form-check-label" htmlFor="femaleGender">Female</label>
                         </div>
 
                         <div className="form-check form-check-inline mb-0 me-4">
-                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender" value="male" {...register('gender')} />
+                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender" value="male" 
+                          {...register('gender',{
+                            required : "Please Select ine Option"
+                          })}/>
                           <label className="form-check-label" htmlFor="maleGender">Male</label>
                         </div>
 
                         <div className="form-check form-check-inline mb-0">
-                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender" value="other" {...register('gender')}/>
+                          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender" value="other" 
+                          {...register('gender',{
+                            required : "Please Select ine Option"
+                          })}/>
                           <label className="form-check-label" htmlFor="otherGender">Other</label>
                         </div>
 
@@ -99,23 +136,33 @@ const Register = () => {
 
                       <div className="row">
                         <div className="col-md-6 mb-4">
-                          <select data-mdb-select-init className='form-control' {...register('state')}>
+                          <select data-mdb-select-init className='form-control' 
+                          {...register('state',{
+                            required : "Select Any State"
+                          })}>
                             <option value="">State</option>
                             <option value="Maharashtra">Maharashtra</option>
                             <option value="Delhi">Delhi</option>
                             <option value="Gujrat">Gujrat</option>
                           </select>
+                          {
+                            errors.state && <div className='text-danger'>{errors.state.message}</div>
+                          }
 
                         </div>
                         <div className="col-md-6 mb-4">
 
-                          <select data-mdb-select-init className='form-control' {...register('city')}>
+                          <select data-mdb-select-init className='form-control' {...register('city',{
+                            required : "Salect Any City"
+                          })}>
                             <option value="">City</option>
                             <option value="Pune">Pune</option>
                             <option value="Delhi">Delhi</option>
                             <option value="Ahemdabad">Ahemdabad</option>
                           </select>
-
+                          {
+                            errors.city && <div className='text-danger'>{errors.city.message}</div>
+                          }
                         </div>
                       </div>
 
@@ -123,7 +170,10 @@ const Register = () => {
                       <div className="row">
                         <div className="col-md-6 mb-4">
 
-                          <select data-mdb-select-init className='form-control' {...register('course')} onChange={courseHandler} value={course}>
+                          <select data-mdb-select-init className='form-control' 
+                          {...register('course',{
+                            required : "Select Any Course"
+                          })} onChange={courseHandler} value={course}>
                             <option value=''>Select Course</option>
                             {
                               Object.keys(courseFees).map((course)=>{
@@ -135,7 +185,9 @@ const Register = () => {
                               })
                             }
                           </select>
-
+                          {
+                            errors.course && <div className='text-danger'>{errors.course.message}</div>
+                          }
                         </div>
                         <div className="col-md-6 mb-4">
                             <input
@@ -149,19 +201,35 @@ const Register = () => {
 
 
                       <div data-mdb-input-init className="form-outline mb-4">
-                        <input type="date" id="form3Example9" className="form-control form-control-lg" {...register('dob')}/>
+                        <input type="date" id="form3Example9" className="form-control form-control-lg" {...register('dob',{
+                          required : "Select Date of Birth"
+                        })}/>
                         <label className="form-label" htmlFor="form3Example9">DOB</label>
                       </div>
 
                       <div data-mdb-input-init className="form-outline mb-4">
-                        <input type="tel" id="form3Example90" className="form-control form-control-lg" {...register('contact')}/>
+                        <input type="tel" id="form3Example90" className="form-control form-control-lg" {...register('contact',{
+                          required : "Enter Valid Contact",
+                          pattern : {
+                            value : /^[6789]{1}[0-9]{9}$/,
+                            message : "Contact must be 10 digit"
+                          }
+                        })}/>
                         <label className="form-label" htmlFor="form3Example90">Contact</label>
+                        {
+                          errors.contact && <div className='text-danger'>{errors.contact.message}</div>
+                        }
                       </div>
 
 
                       <div data-mdb-input-init className="form-outline mb-4">
-                        <input type="email" id="form3Example97" className="form-control form-control-lg" {...register('email')}/>
+                        <input type="email" id="form3Example97" className="form-control form-control-lg" {...register('email',{
+                          required : "Enter valid email"
+                        })}/>
                         <label className="form-label" htmlFor="form3Example97">Email ID</label>
+                        {
+                          errors.email && <div className='text-danger'>{errors.email.message}</div>
+                        }
                       </div>
 
                       <div className="d-flex justify-content-end pt-3">
